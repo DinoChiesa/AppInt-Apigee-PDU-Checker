2f2778b4dc1e2137c6e3b6368fe1e3f699e0bbf5
DinoChiesa <dchiesa@google.com>
2025-02-21 22:32:46 +0000

    fix: actually include SCHEDULE in env.sh

--- env.sh
+++ env.sh
@@ -3,3 +3,5 @@ export REGION=us-west1
 export APIGEE_PROJECTS=your-apigee-project-1,your-apigee-project-2
 export EXAMPLE_NAME=apigee-pdu-report
 export EMAIL_ADDR=person@example.com
+# try https://crontab.guru/#51_21_*_*_*
+export SCHEDULE="51 21 * * *"
